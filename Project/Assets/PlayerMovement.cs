677901ac4bb808795566f3c94df80526cda54c86
Rigre R. Garciandia <rigrergl@gmail.com>
2020-10-25 15:37:09 -0500

    Added some player movement

---
+++ Project/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 200f;
+
+    public Rigidbody2D rb;
+
+    public Transform tranform;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x != 0)
+            transform.localScale = new Vector3(-1*movement.x,1f,1f);
+
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
